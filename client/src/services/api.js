74331b1d0432bba0492b10ecb38c319a8b2abf1c
Uttam Kumar <uttamkssm2000.com>
2026-07-01 14:49:58 +0530

    Commit done

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "/api",
-//   withCredentials: true, 
-// });
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   timeout: 30000,
